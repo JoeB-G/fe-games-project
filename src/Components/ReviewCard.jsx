1d235947341cc21d998a8cf0665e685572514b3c
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-04-18 14:03:17 +0100

    change from useNavigate to Link for accessibility

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -2,19 +2,14 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import moment from "moment";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ReviewCard = ({ review }) => {
-  const navigate = useNavigate()
 
-  const handleClick = () => {
-    navigate(`/reviews/${review.review_id}`)
-  }
   return (
     <Card sx={{}}>
-      <CardActionArea onClick={handleClick}>
+      <Link to={`/reviews/${review.review_id}`}>
         <CardMedia
           component="img"
           height="140"
@@ -33,7 +28,7 @@ const ReviewCard = ({ review }) => {
             Posted {moment(review.created_at).format("DD/MM/YYYY HH:mm")}
           </Typography>
         </CardContent>
-      </CardActionArea>
+      </Link>
     </Card>
   );
 };
